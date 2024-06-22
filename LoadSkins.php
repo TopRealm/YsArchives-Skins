3e74fda535ca384efc0ededcade99d308adef001
ZoruaFox <zorua@vip.qq.com>
2024-06-23 00:15:38 +0800

    chore: update .gitignore file

---
+++ LoadSkins.php
@@ -0,0 +1,7 @@
+<?php
+wfLoadSkin( 'Citizen', "/www/wwwroot/mw-utils/YsArchives-Skins/Citizen/skin.json" );
+wfLoadSkin( 'Vector', "/www/wwwroot/mw-utils/YsArchives-Skins/Vector/skin.json" );
+wfLoadSkin( 'Gongbi', "/www/wwwroot/mw-utils/YsArchives-Skins/Gongbi/skin.json" );
+
+$wgDefaultSkin = "Citizen";
+$wgCitizenSearchDescriptionSource = "textextracts";
