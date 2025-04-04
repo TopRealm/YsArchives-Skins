7bc1010d219a62a069e784e2e61c3e8534c24404
海屿有燕 <zorua@vip.qq.com>
2025-04-05 00:29:36 +0800

    恢复加载 Gongbi 皮肤的代码

--- LoadSkins.php
+++ LoadSkins.php
@@ -1,7 +1,7 @@
 <?php
 wfLoadSkin( 'Citizen', "/www/wwwroot/mw-utils/YsArchives-Skins/Citizen/skin.json" );
 wfLoadSkin( 'Vector', "/www/wwwroot/mw-utils/YsArchives-Skins/Vector/skin.json" );
-#wfLoadSkin( 'Gongbi', "/www/wwwroot/mw-utils/YsArchives-Skins/Gongbi/skin.json" );
+wfLoadSkin( 'Gongbi', "/www/wwwroot/mw-utils/YsArchives-Skins/Gongbi/skin.json" );
 
 $wgDefaultSkin = "Citizen";
 $wgCitizenSearchDescriptionSource = "pagedescription";
