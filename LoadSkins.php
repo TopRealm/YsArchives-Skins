38f57f4b8b98f124b3cd568fa1aa2e5828e7045c
海屿有燕 <zorua@vip.qq.com>
2025-04-06 21:13:12 +0800

    添加 wfLoadSkin 伪定义

--- LoadSkins.php
+++ LoadSkins.php
@@ -1,4 +1,9 @@
 <?php
+if (!function_exists('wfLoadSkin')) {
+    function wfLoadSkin($skinName, $path = null) {
+        // 伪定义，仅用于静态分析工具
+    }
+}
 wfLoadSkin( 'Citizen', "/www/wwwroot/mw-utils/YsArchives-Skins/Citizen/skin.json" );
 wfLoadSkin( 'Vector', "/www/wwwroot/mw-utils/YsArchives-Skins/Vector/skin.json" );
 wfLoadSkin( 'Gongbi', "/www/wwwroot/mw-utils/YsArchives-Skins/Gongbi/skin.json" );
